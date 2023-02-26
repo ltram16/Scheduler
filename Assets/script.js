7856da78ff9fe3d5b1891f508583312c5b4ce667
ltram16 <ltram16@gmail.com>
2023-02-26 10:31:24 -0800

    modified html and css

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,29 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  var currentDay = $("#currentDay");
+  var currentHour = moment().format("HH");
+  console.log(currentHour);
+
+  var date = moment().format("dddd, MMMM Do YYYY");
+  currentDay.text(date);
+  var currentTIme = $("<p>");
+  currentTime.text(moment().format("h:mm a"));
+  currentTime.attr("class", "time");
+  currentDay.append(currentTime);
+
+  function colorChange() { //will change color of the text area based on past, present, future
+    
+    for (var i = 10 ; i <= 18 ; i++) {
+
+        var hourID = $("#" + i).attr("id")
+        var textArea = $("#" + i)
+
+        if (currentHour > hourID) {
+            textArea.css("backgroundColor", "lightgrey"); //color for past
+
+        } else if (currentHour === hourID) {
+            textArea.css("backgroundColor", "#CCCCFF"); //color for present time
+        } else if (currentHour < hourID) {
+            textArea.css("backgroundColor", "pink"); //color for future
+        }
+    }
+}
+colorChange(); //starts when page loads
